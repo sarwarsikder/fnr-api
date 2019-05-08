089f612d8673b6d702e7c278524160d5385fe06e
Mahedi Hasan <mahedi302@gmail.com>
2019-05-08 15:13:08 +0600

    add login api

--- serviceapp/urls.py
+++ serviceapp/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import include
-from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('serviceapp.urls')),
     path('auth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
