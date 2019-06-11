fa5b9fb31c01208d8bd6b88a319b434d59efe689
Iftekhar <iftekhar@workspaceit.com>
2019-06-11 18:46:39 +0600

    Push Notification Fix

--- pushnotificationapp/signals.py
+++ pushnotificationapp/signals.py
@@ -2,19 +2,17 @@ from pywebpush import webpush, WebPushException
 import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from adminapp.models import Users
 from pushnotificationapp.models import PushNotification, Subscribers
 import json
-import os
-import datetime
+from pyfcm import FCMNotification
 from notifications.signals import notify
 
 
 def notification_sender(subscription_info, data):
     count = 0
     VAPID_CLAIMS = {
-        "exp":  1212121212,
+        "exp": 1212121212,
         "sub": "mailto:iftekhar@workspaceit.com"
     }
 
@@ -24,7 +22,7 @@ def notification_sender(subscription_info, data):
             subscription_info=subscription_info,
             data=data,
             vapid_private_key="bF0oatL1tc0-vPr_9Lx8VMUmXA4Fgp_dJaQMpZk25ag",
-            vapid_claims = VAPID_CLAIMS
+            vapid_claims=VAPID_CLAIMS
         )
         count += 1
     except WebPushException as e:
@@ -35,10 +33,15 @@ def notification_sender(subscription_info, data):
 def send_notification(sender, **kwargs):
     subscribers = Subscribers.objects.filter(user_id=kwargs['instance'].recipient)
     for subscriber in subscribers:
-        subscription_info = {"endpoint": subscriber.endpoint,"keys": json.loads(subscriber.keys)}
-        notification_sender(subscription_info, kwargs['instance'].message)
+        if subscriber.device == "mobile":
+            push_service = FCMNotification(api_key="AIzaSyDe760rQNsg6JOJbohxdrYTW86E9FtDSyw")
+            registration_id = "d7mMlK4FKR4:APA91bGCnJ-fPmlmvRDJTnTBTeg7-ZSeUh-puqrSHdAShkNIZEJrF5c2pTcadmU5rioks8aOIwek9oFKCxI0jIJXC1I19gMhyyD3gnGOU0eTeo8SDjkGTejxjYyDYww7d5t77yqsruUs"
+            result = push_service.notify_single_device(registration_id=registration_id,
+                                                       message_title=kwargs['instance'].title,
+                                                       message_body=kwargs['instance'].message)
+
+        else:
+            subscription_info = {"endpoint": subscriber.endpoint, "keys": json.loads(subscriber.keys)}
+            notification_sender(subscription_info, kwargs['instance'].message)
         user = Users.objects.filter(id=kwargs['instance'].recipient)[0]
-        notify.send(user, recipient=user, verb='you reached level 10')
-
-
-
+        notify.send(user, recipient=user, verb=kwargs['instance'].message)
