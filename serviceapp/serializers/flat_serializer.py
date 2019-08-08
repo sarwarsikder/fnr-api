ea19373eacc6d06e51c514a307907d3e729d0a49
Mahedi Hasan <mahedi302@gmail.com>
2019-08-08 18:42:48 +0600

    send status with components

--- serviceapp/serializers/flat_serializer.py
+++ serviceapp/serializers/flat_serializer.py
@@ -1,15 +1,25 @@
+from datetime import datetime
+
 from django.conf import settings
 from rest_framework import serializers
-from adminapp.models import Flats, FlatPlans
+from adminapp.models import Flats, FlatPlans, Tasks
 
 
 class FlatSerializer(serializers.ModelSerializer):
     total_tasks = serializers.IntegerField(read_only=True, default=None)
     tasks_done = serializers.IntegerField(read_only=True, default=None)
+    status = serializers.SerializerMethodField()
 
     class Meta:
         model = Flats
-        fields = ('id', 'number', 'description', 'client_name', 'client_address', 'client_email', 'client_tel', 'total_tasks', 'tasks_done')
+        fields = ('id', 'number', 'description', 'client_name', 'client_address', 'client_email', 'client_tel', 'total_tasks', 'tasks_done', 'status')
+
+    def get_status(self, flat):
+        status = "other"
+        current_date = datetime.now()
+        if Tasks.objects.filter(building_component__flat_id=flat.id, due_date__lt=current_date).exclude(status='done').exists():
+            status = "overdue"
+        return status
 
 
 class FlatPlanSerializer(serializers.ModelSerializer):
