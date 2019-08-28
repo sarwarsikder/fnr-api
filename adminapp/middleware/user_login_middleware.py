ef3cdd2d7edf1393fe8d8d71c464d3f392a06c04
Mahedi Hasan <mahedi302@gmail.com>
2019-08-28 18:48:49 +0600

    update sidebar

--- adminapp/middleware/user_login_middleware.py
+++ adminapp/middleware/user_login_middleware.py
@@ -18,51 +18,51 @@ class UserLoginMiddleware(generic.DetailView):
 
     def __call__(self, request):
         path = request.path.split('/')[1]
-        if path == '' and path != 'api' and path != 'media':
+        if path != 'api' and path != 'media' and path != 'login' and path != 'forget-password' and path != 'reset-password' and path != 'privacy_policy':
             if request.is_ajax() == False:
                 # settings.USE_TZ = False
                 browser_current_url = resolve(request.path_info).url_name
-                if browser_current_url != 'login' and browser_current_url != 'forget-password' and browser_current_url != 'reset-password':
-                    if not request.user.is_authenticated:
-                        return redirect('login')
-                    if request.user.is_superuser:
-                        current_projects = list(Projects.objects.filter(is_complete=False).order_by('-id').values('id', 'name'))
-                    else:
-                        current_projects = list(Projects.objects.filter(is_complete=False, projectstuff__user_id=request.user.id).order_by('-id').values('id', 'name').distinct())
-                    request.session["current_projects"] = current_projects
-                    if request.user.current_activity:
-                        current_activity = json.loads(request.user.current_activity)
-                        if 'project_id' in current_activity:
-                            request.session["active_project"] = {
-                                'id': current_activity['project_id'],
-                                'name': current_activity['project_name']
-                            }
-                        if 'building_id' in current_activity:
-                            request.session["active_building"] = {
-                                'id': current_activity['building_id'],
-                                'number': current_activity['building_number']
-                            }
-                        if 'flat_id' in current_activity:
-                            request.session["active_flat"] = {
-                                'id': current_activity['flat_id'],
-                                'number': current_activity['flat_number']
-                            }
-                    if 'active_project' not in request.session:
+                # if browser_current_url != 'login' and browser_current_url != 'forget-password' and browser_current_url != 'reset-password':
+                if not request.user.is_authenticated:
+                    return redirect('login')
+                if request.user.is_superuser:
+                    current_projects = list(Projects.objects.filter(is_complete=False).order_by('-id').values('id', 'name'))
+                else:
+                    current_projects = list(Projects.objects.filter(is_complete=False, projectstuff__user_id=request.user.id).order_by('-id').values('id', 'name').distinct())
+                request.session["current_projects"] = current_projects
+                if request.user.current_activity:
+                    current_activity = json.loads(request.user.current_activity)
+                    if 'project_id' in current_activity:
                         request.session["active_project"] = {
-                            'id': '',
-                            'name': ''
+                            'id': current_activity['project_id'],
+                            'name': current_activity['project_name']
                         }
-                    if 'active_building' not in request.session:
+                    if 'building_id' in current_activity:
                         request.session["active_building"] = {
-                            'id': '',
-                            'number': ''
+                            'id': current_activity['building_id'],
+                            'number': current_activity['building_number']
                         }
-                    if 'active_flat' not in request.session:
+                    if 'flat_id' in current_activity:
                         request.session["active_flat"] = {
-                            'id': '',
-                            'number': ''
+                            'id': current_activity['flat_id'],
+                            'number': current_activity['flat_number']
                         }
-                    request.session.modified = True
+                if 'active_project' not in request.session:
+                    request.session["active_project"] = {
+                        'id': '',
+                        'name': ''
+                    }
+                if 'active_building' not in request.session:
+                    request.session["active_building"] = {
+                        'id': '',
+                        'number': ''
+                    }
+                if 'active_flat' not in request.session:
+                    request.session["active_flat"] = {
+                        'id': '',
+                        'number': ''
+                    }
+                request.session.modified = True
         # elif path != 'api':
         #     if request.is_ajax() == False:
         #         if 'user_bikeshare_settings' not in request.session:
